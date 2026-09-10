536a1cc41148fe3536a98445bc6571067f11547f
rohithvairavel-ctrl <rohithvairavel01@gmail.com>
2026-09-11 05:15:24 +0530

    feat: Qini/AUUC metrics module

---
+++ src/causal_uplift/metrics/__init__.py
@@ -0,0 +1,9 @@
+from .qini import auuc, evaluate_scores, qini_coefficient, qini_curve, uplift_by_decile
+
+__all__ = [
+    "qini_curve",
+    "auuc",
+    "qini_coefficient",
+    "uplift_by_decile",
+    "evaluate_scores",
+]
